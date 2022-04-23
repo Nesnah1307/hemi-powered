69cd0ae2f60b1456c3e918bad093f06d14545307
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-04-23 16:12:48 -0600

    seeded

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+('Mack', 'Hansen', 01),
+('Gus', 'Guapo', 02),
+('Payton', 'Swimmer', 03),
+('Kody', 'Mastic', 04),
+('Lyil', 'Teflon', 05),
+('Jayson', 'Great', 06);
+
+
+INSERT INTO role (job, base_salary, departmentId) 
+VALUES
+('Service Technician', 70000, 1),
+('Plumber', 69000, 2),
+('Service Manager', 90000, 3);
+
+
+INSERT INTO department
+VALUES
+('HVAC'),
+('Plumbing'),
+('Manager')
